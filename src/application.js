419c735a417b03d93423372a429a57d934f67e1f
Raman Marozau <engineer.morozov@gmail.com>
2016-11-30 23:02:31 +0300

    Removed unused configs Removed flow artifact

--- src/application.js
+++ src/application.js
@@ -3,18 +3,17 @@
 import React, { Component, PropTypes } from 'react';
 import { render } from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
-import { Subject } from 'rxjs/Subject';
+
 import assign from 'lodash-es/assign';
 
 import { Page } from './components/Page';
-
 import { initApplicationStreams } from './streams';
 
 import './main.scss';
 
 class Application extends Component {
 
-  event$$: ?Subject = void 0;
+  event$$ = void 0;
 
   constructor(props) {
     super(props);
@@ -26,7 +25,6 @@ class Application extends Component {
 
     return (
       <div className="Application">
-
         <Page event$$={event$$}>
           {this.props.children}
         </Page>
