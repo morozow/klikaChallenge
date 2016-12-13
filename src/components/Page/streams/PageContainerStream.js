434a17c39e1455a39fa32d9706e83b8322c8ef72
Raman Marozau <engineer.morozov@gmail.com>
2016-12-13 20:15:51 +0300

    Added View Types to PageContainer$$

--- src/components/Page/streams/PageContainerStream.js
+++ src/components/Page/streams/PageContainerStream.js
@@ -1,9 +1,19 @@
 import { Subject } from 'rxjs/Subject';
 import { PageContainerDefer, PageSettings } from 'components/Page';
 
+import { filter as filter$ } from 'rxjs/operator/filter';
+
+export const VIEWS_TYPES = {
+  grid: 'grid',
+  view: 'view',
+};
+export const PAGE_CONTAINER_VIEWS = [VIEWS_TYPES.grid, VIEWS_TYPES.view];
+
 export const pageContainer$$ = new Subject();
-pageContainer$$.subscribe(({ callback, ...props }: PageSettings) => {
-  PageContainerDefer
-    .create(props)
-    .then((Container) => callback(null, Container));
-});
+pageContainer$$
+  ::filter$(({ view }) => PAGE_CONTAINER_VIEWS.includes(view))
+  .subscribe(async ({ callback, ...props }: PageSettings) => {
+    PageContainerDefer
+      .create(props)
+      .then((Container) => callback(null, Container));
+  });
